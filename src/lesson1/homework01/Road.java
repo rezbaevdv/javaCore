dc0d580fc442e66f0b791ca6f9e62515d3267ecc
Rezbaevdv <rezbaevdv@gmail.com>
2021-03-20 01:52:51 +0500

    Home Work 01

---
+++ src/lesson1/homework01/Road.java
@@ -0,0 +1,9 @@
+package lesson1.homework01;
+
+public class Road {
+    public int length;
+
+    public void anyRun(CanRun any) {
+        any.run(length);
+    }
+}
---
+++ src/lesson1/homework01/Wall.java
@@ -0,0 +1,8 @@
+package lesson1.homework01;
+
+public class Wall {
+    public int height;
+    public void anyJumps(CanJump any) {
+        any.jump(height);
+    }
+}
