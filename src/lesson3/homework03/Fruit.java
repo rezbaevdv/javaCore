12a3c9f23e81d282d0f52d8856e05773c44eb8e4
Rezbaevdv <rezbaevdv@gmail.com>
2021-03-29 00:10:20 +0500

    Дз 03

---
+++ src/lesson3/homework03/Fruit.java
@@ -0,0 +1,13 @@
+package lesson3.homework03;
+
+public abstract class Fruit {
+    float volume;
+
+    public Fruit (float volume) {
+        this.volume = volume;
+    }
+
+    public float getVolume() {
+        return volume;
+    }
+}
